4af3fcba611042c641b9134832075e9572d44d06
abuzar08 <31164428+abuzar08@users.noreply.github.com>
2020-05-16 12:13:37 +0530

    New Git repo for udemy JVS

---
+++ Theory.md
@@ -0,0 +1,10 @@
+# Udemy
+
+#### Places to add javascript
+- script tag
+- within containers
+- devtool console in browser
+- from external file (src = "file.js")
+
+The last is best because flexibility, modularity.
+Plus a lot of htmls can call on this.
---
+++ script.js
@@ -0,0 +1,23 @@
+// document.write("Hello World!")
+// alert('Yo!')
+
+// var myStr = "Yo!";
+// var myNum = 100;
+// var myBool = true;
+
+// var question1 = "What\'s";
+// var question2 = " your name?";
+// var answer = prompt(question1 + question2);
+// alert(`Hello ${answer}!`)
+// document.write(`Your name is ${answer}!`)
+
+var a = Number(prompt('Enter first number'));
+var b = Number(prompt('Enter second number'));
+
+// a = Number(a);
+// b = Number(b);
+var sum = a+b;
+var diff = a-b;
+
+console.log(`Sum = ${sum}`);
+console.log(`Difference = ${diff}`);
